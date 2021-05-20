68ad2b8fded587becf8b96b3d8cb21919f410d4c
Luca Beetz <luca-beetz@tutanota.com>
2021-05-20 15:19:27 +0200

    Implement London baseline

--- a5/src/london_baseline.py
+++ a5/src/london_baseline.py
@@ -2,3 +2,10 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+import utils
+
+line_count = len(open('birth_dev.tsv', 'r').readlines())
+preds = ['London'] * line_count
+total, correct = utils.evaluate_places('birth_dev.tsv', preds)
+print('Correct: {} out of {}: {}%'.format(
+    correct, total, correct/total*100))
