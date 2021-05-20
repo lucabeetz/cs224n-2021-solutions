aa97fa8b9fda261351be58a58bcd00ff51a1d465
Luca Beetz <luca-beetz@tutanota.com>
2021-05-20 18:12:56 +0200

    Try different CorruptedDataset version

--- a5/src/dataset.py
+++ a5/src/dataset.py
@@ -172,27 +172,70 @@ class CharCorruptionDataset(Dataset):
 
     def __getitem__(self, idx):
         # TODO [part e]: see spec above
+        # document = self.data[idx]
+        # len_trunc = random.randrange(4, int(self.block_size*7/8))
+        # len_trunc = min(len(document), len_trunc)
+        # document = document[:len_trunc]
+
+        # len_masked = random.randrange(int(1/8*len_trunc), int(3/8*len_trunc))
+        # len_prefix = random.randrange(1, len_trunc - len_masked - 1)
+        # assert len_trunc >= 4
+
+        # prefix = document[:len_prefix]
+        # masked = document[len_prefix:len_prefix+len_masked]
+        # suffix = document[len_prefix+len_masked:]
+
+        # masked_string = prefix + self.MASK_CHAR + suffix + self.MASK_CHAR + \
+        #     masked + self.PAD_CHAR * (self.block_size - len_trunc - 2)
+        # assert len(masked_string) == self.block_size
+
+        # x = masked_string[:-1]
+        # y = masked_string[1:]
+
+        # x = torch.LongTensor([self.stoi[c] for c in x])
+        # y = torch.LongTensor([self.stoi[c] for c in y])
+        # return x, y
+
+        # TODO [part e]: see spec above
         document = self.data[idx]
-        len_trunc = random.randint(4, int(self.block_size*7/8))
-        len_trunc = min(len(document), len_trunc)
-        document = document[:len_trunc]
+        # 1. randomly truncate to [4, 7/8 * block_size]
+        doc_len = len(document)
+        truncate_len = random.randint(4, int(self.block_size * 7 / 8))
+        truncate_len = min(doc_len, truncate_len)
+        truncated_doc = document[:truncate_len]
+        # 2. break to [prefix] [masked_content] [suffix]
+        masked_len = random.randint(
+            int(1 / 8 * truncate_len), int(3 / 8 * truncate_len))
+        assert truncate_len >= 4, (doc_len, truncate_len,
+                                   masked_len, document, idx)
+        prefix_len = random.randint(1, truncate_len - masked_len - 1)
 
-        len_masked = random.randint(
-            int(1/8*len_trunc), int(3/8*len_trunc))
-        len_prefix = random.randint(1, len_trunc - len_masked - 1)
+        prefix = truncated_doc[:prefix_len]
+        masked_content = truncated_doc[prefix_len:prefix_len + masked_len]
+        suffix = truncated_doc[prefix_len + masked_len:]
 
-        prefix = document[:len_prefix]
-        masked = document[len_prefix:len_prefix+len_masked]
-        suffix = document[len_prefix+len_masked:]
+        # 3. rearrange to masked_string: [prefix] MASK_CHAR [suffix] MASK_CHAR [masked_content] [pads]
+        masked_string = prefix + self.MASK_CHAR + suffix + self.MASK_CHAR + masked_content + self.PAD_CHAR * (
+            self.block_size - truncate_len - 2)
+        assert len(masked_string) == self.block_size
 
-        masked_string = prefix + self.MASK_CHAR + suffix + self.MASK_CHAR + \
-            masked + self.PAD_CHAR * (self.block_size - len_trunc - 2)
-
+        # 4. input = masked_string[:-1], output = masked_string[1:]
         x = masked_string[:-1]
         y = masked_string[1:]
 
+        # 5. encode to Long tensors
         x = torch.LongTensor([self.stoi[c] for c in x])
         y = torch.LongTensor([self.stoi[c] for c in y])
+        return x, y
+
+        inp, oup = self.data[idx].split('\t')
+        x = inp + self.MASK_CHAR + oup + self.MASK_CHAR
+        x = x + self.PAD_CHAR * (self.block_size - len(x))
+        y = self.PAD_CHAR * (len(inp) - 1) + x[len(inp):]
+
+        x = x[:-1]
+        x = torch.tensor([self.stoi[c] for c in x], dtype=torch.long)
+        y = torch.tensor([self.stoi[c] for c in y], dtype=torch.long)
         return x, y
 
 
